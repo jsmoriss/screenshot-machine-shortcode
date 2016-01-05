837e7201d52599f07c0517b80ce0c2c28c44ac62
JS Morisset <jsm@surniaulula.com>
2016-01-04 19:40:01 -0700

    v1.1

---
+++ readme.txt
@@ -0,0 +1,97 @@
+=== JSM's Screenshot Machine Shortcode ===
+Plugin Name: JSM's Screenshot Machine Shortcode
+Plugin Slug: screenshot-machine-shortcode
+Contributors: jsmoriss
+Tags: screenshot, machine, shortcode
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.txt
+Requires At Least: 3.0
+Tested Up To: 4.4
+Stable Tag: 1.1
+
+Shortcode to include images from Screenshot Machine in your content.
+
+== Description ==
+
+Use the `&#91;ssm&#93;` shortcode in your content with the following arguments:
+
+* key="{account key}"
+* url="{website url}"
+* size="{size letter}" (default=T)
+* format="{jpg|png|gif}" (default=jpg)
+* days="{cache expiration}" (default=14)
+* wait="{wait in ms}" (default=200)
+* title="{href title}"
+* link="{yes|no}" (default=yes)
+* target="{link target}" (default=_blank)
+* refresh="{yes|no}" (default=yes)
+
+<!--more-->
+
+You can find the {account key} on [your Screenshot Machine account/settings page](https://www.screenshotmachine.com/account.php).
+
+The {website url} is the web page URL to capture into a screenshot.
+
+Valid {size letter} values are:
+
+* T = Width 120 x Height 90
+* S = Width 200 x Height 150
+* E = Width 320 x Height 240
+* N = Width 400 x Height 300
+* M = Width 640 x Height 480
+* L = Width 800 x Height 600
+* X = Width 1024 x Height 768
+
+{jpeg|png|gif} is the image format to use (default is jpg).
+
+{cache expiration} is the number of days a screenshot should be used before a new one is created.
+
+{wait in ms} is the number of milliseconds to wait before capturing the screenshot.
+
+{href title} is the title text for the image alt and link title.
+
+The link yes/no value will determine if the image is linked to the web page URL or not.
+
+The {link target} default will open links in a new window/tab. An empty string, '_self', '_top', '_parent', and a framename are also valid.
+
+The refresh parameter includes javascript to retry the image every second until it's available (for a maximum of 10 seconds).
+
+Example:
+
+`
+&#91;ssm key="abc123" url="http://surniaulula.com/extend/plugins/screenshot-machine-shortcode/" size="S"&#93;
+`
+
+== Installation ==
+
+*Automated Install*
+
+1. Go to the wp-admin/ section of your website
+1. Select the *Plugins* menu item
+1. Select the *Add New* sub-menu item
+1. In the *Search* box, enter the plugin name
+1. Click the *Search Plugins* button
+1. Click the *Install Now* link for the plugin
+1. Click the *Activate Plugin* link
+
+*Semi-Automated Install*
+
+1. Download the plugin archive file
+1. Go to the wp-admin/ section of your website
+1. Select the *Plugins* menu item
+1. Select the *Add New* sub-menu item
+1. Click on *Upload* link (just under the Install Plugins page title)
+1. Click the *Browse...* button
+1. Navigate your local folders / directories and choose the zip file you downloaded previously
+1. Click on the *Install Now* button
+1. Click the *Activate Plugin* link
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= Version 1.1 =
+
+* Removed the 'blank' argument, replacing it with 'target' instead.
+* Added the 'format', 'days', and 'wait' arguments.
+
---
+++ ssm-shortcode.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * Plugin Name: JSM's Screenshot Machine Shortcode
+ * Plugin URI: http://surniaulula.com/extend/plugins/screenshot-machine-shortcode/
+ * Author: Jean-Sebastien Morisset
+ * Author URI: http://surniaulula.com/
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl.txt
+ * Description: Shortcode for Screenshot Machine Images
+ * Requires At Least: 3.0
+ * Tested Up To: 4.4
+ * Version: 1.1
+ *
+ * This script is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This script is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details at
+ * http://www.gnu.org/licenses/.
+ *
+ * Copyright 2013-2016 Jean-Sebastien Morisset (http://surniaulula.com/)
+ */
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ScreenShotMachineShortCode' ) ) {
+
+	class ScreenShotMachineShortCode {  
+
+		protected $api_url = 'http://api.screenshotmachine.com/';  
+
+		public function __construct()  {  
+			add_shortcode('ssm', array($this, 'shortcode'));
+		}  
+
+		public function shortcode($atts, $content = NULL){
+
+			extract( shortcode_atts( array(
+				'key' => '',
+				'url' => '',
+				'size' => 'T',
+				'format' => 'jpg',
+				'days' => '14',
+				'wait' => '200',
+				'title' => '',
+				'link' => true,
+				'target' => '_blank',
+				'refresh' => true,
+			), $atts ) );
+
+			$size = strtoupper( $size );
+			$link = filter_var( $link, FILTER_VALIDATE_BOOLEAN );
+			$refresh = filter_var( $refresh, FILTER_VALIDATE_BOOLEAN );
+
+			switch ( $size ) {
+				case 'T' : $width=120; $height=90; break;
+				case 'S' : $width=200; $height=150; break;
+				case 'E' : $width=320; $height=240; break;
+				case 'N' : $width=400; $height=300; break;
+				case 'M' : $width=640; $height=480; break;
+				case 'L' : $width=800; $height=600; break;
+				case 'X' : $width=1024; $height=768; break;
+			}
+
+			$output = '';
+			$classnames = array( 'ssm' );
+			$img_url = $this->api_url.'?key='.$key.
+				'&url='.urlencode($url).
+				'&size='.$size.
+				'&format='.$format.
+				'&cacheLimit='.$days.
+				'&timeout='.$wait;
+
+			if ( $refresh )  {
+			    	$classnames[] = 'ssm_refresh';
+				wp_register_script( 'ssm_refresh', plugins_url( 'ssm-shortcode.js' , __FILE__ ) ); 
+   					wp_enqueue_script( 'ssm_refresh', array('jquery'), '1.0.0', TRUE );
+			}
+
+			if ( $link == true ) {
+				$output .= '<a href="'. $url .'" title="'. $title .'" class="ssm_link" ';
+				if ( ! empty( $target ) ) 
+					$output .= ' target="' . $target . '" ';
+				$output .= ' >';
+			}
+
+			$output .= '<img alt="'.$title.'" class="'.implode (' ', $classnames).'" data-refreshcounter="0" 
+				data-src="'.$img_url.'" data-width="'.$width.'" data-height="'.$height.'" 
+				src="'.$img_url.'" width="'.$width.'" height="'.$height.'" />';
+
+			if ( $link == true )
+				$output .= '</a>';
+
+			return $output;
+		}  
+	}	 
+
+	$_ScreenShotMachineShortCode = new ScreenShotMachineShortCode(); 
+}
+
+?>
