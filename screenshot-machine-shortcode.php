90499e5f701ac2fe0cfbea2b3d717f4dbef2ec4d
JS Morisset <jsm@surniaulula.com>
2017-03-25 00:31:52 -0700

    v1.1.2-1

--- screenshot-machine-shortcode.php
+++ screenshot-machine-shortcode.php
@@ -10,7 +10,7 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Shortcode for Screenshot Machine Images
- * Requires At Least: 3.8
+ * Requires At Least: 3.7
  * Tested Up To: 4.7.3
  * Version: 1.1.2-1
  *
