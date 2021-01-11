4428251cd2871e6550c89b0c703bf528dcf2d136
JS Morisset <jsm@surniaulula.com>
2021-01-10 16:41:10 -0800

    v2.2.0

--- screenshot-machine-shortcode.js
+++ screenshot-machine-shortcode.js
@@ -1,38 +1,38 @@
-jQuery(document).ready(function () {
+jQuery( document ).on( 'ready', function(){
 
 	SSMSC_Refresh_IntervalId = self.setInterval(		
 
-		function SSMSC_Refresh () {
+		function SSMSC_Refresh(){
 
 			var anotherCheckIsNeeded = false;
 
-			jQuery('.ssm_refresh').each(
+			jQuery( '.ssm_refresh' ).each(
 
-					function (index, element) {
+					function ( index, element ) {
 
 						var a=new Image();
 
-						a.src=jQuery(element).attr('src');
+						a.src=jQuery( element ).attr( 'src' );
 
-						if (a.width != jQuery(element).attr('data-width')) {
+						if ( a.width != jQuery( element ).attr( 'data-width' ) ) {
 
-							jQuery(element).attr('src', jQuery(element).attr('data-src')+'&ts='+Math.random(1000));
+							jQuery( element ).attr( 'src', jQuery( element ).attr( 'data-src' ) + '&ts=' + Math.random( 1000 ) );
 
 							anotherCheckIsNeeded = true;
 
-							jQuery(element).attr('data-refreshcounter', jQuery(element).attr('data-refreshcounter')+1);
+							jQuery( element ).attr( 'data-refreshcounter', jQuery( element ).attr( 'data-refreshcounter' ) + 1 );
 						}
 
-						if (jQuery(element).attr('data-refreshcounter') >= 10) {
+						if ( jQuery( element ).attr( 'data-refreshcounter' ) >= 10 ) {
 
 							anotherCheckIsNeeded = false;
 						}
 					}
 			);
 
-			if (anotherCheckIsNeeded === false){
+			if ( anotherCheckIsNeeded === false ){
 
-				self.clearInterval(SSMSC_Refresh_IntervalId);
+				self.clearInterval( SSMSC_Refresh_IntervalId );
 			}
 
 		}, 10000
