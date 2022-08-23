f8eb01e861464f2a52bdcfde13784f68302b3d06
JS Morisset <jsm@surniaulula.com>
2022-08-23 10:06:38 -0700

    v2.3.0

--- screenshot-machine-shortcode.php
+++ screenshot-machine-shortcode.php
@@ -12,7 +12,7 @@
  * Description: Shortcode for Screenshot Machine Images
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 6.0.1
+ * Tested Up To: 6.0.2
  * Version: 2.3.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
